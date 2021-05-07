4f875bdc7032810cedd4562b02a6ee0b5791b9be
Nicholas Jordan <NicholasPhJordan@gmail.com>
2021-05-07 11:53:34 -0500

    Started setting up bread crumb behavior

---
+++ Assets/Scripts/BreadCrumbs/BreadCrumbBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreadCrumbBehavior : MonoBehaviour
+{
+    float score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
