6ef6dbc8339b396a66ae3145a1afa295c7f0e864
Cailey B <cailey.m.bianchini@gmail.com>
2021-05-12 10:57:50 -0500

    started implementing new unity input

---
+++ Assets/scripts/Input/InputEventBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputEventBehaviour : MonoBehaviour
+{
+
+    private PlayerMovementBehaviour _movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _movement = GetComponent<PlayerMovementBehaviour>();
+    }
+
+
+}
