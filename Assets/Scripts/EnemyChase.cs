b84012f1e7a3023e3b801bb9b8be1f8cadfadd66
Joshua Albe <joshjalbe@gmail.com>
2021-05-12 16:36:41 -0500

    Worked on the chase behavior

---
+++ Assets/Scripts/EnemyChase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyChase : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    [Tooltip("The object the enemy will be seeking towards.")]
+    [SerializeField]
+    private GameObject _target;
+    private NavMeshAgent _agent;
+
+    public GameObject Target
+    {
+        get
+        {
+            return _target;
+        }
+        set
+        {
+            _target = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get a reference to the attached rigidbody
+        _rigidbody = GetComponent<Rigidbody>();
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void FixedUpdate()
+    {
+        //If a target hasn't been set return
+        if (!_target)
+            return;
+
+        _agent.SetDestination(_target.transform.position);
+    }
+}
