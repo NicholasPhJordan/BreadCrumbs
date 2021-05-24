dc714dfd6a86670d7d623f6ad7cc7afb42bbf0ab
Cailey B <cailey.m.bianchini@gmail.com>
2021-05-24 12:50:53 -0500

    fixed the conflicts for player movement

--- Assets/scripts/Input/PlayerControls.cs
+++ Assets/scripts/Input/PlayerControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/scripts/Input/PlayerControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input/PlayerControls.inputactions'
 
 using System;
 using System.Collections;
