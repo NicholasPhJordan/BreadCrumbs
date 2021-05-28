f6cc0ef23785f386a54c60d43968c124ea4335d2
Nicholas Jordan <NicholasPhJordan@gmail.com>
2021-05-28 08:26:28 -0500

    created  a scene to test collectibles

---
+++ Assets/Scripts/TestMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    public GameObject player;
+    public float playerSpeed = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = (GameObject)this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            player.transform.position += player.transform.forward * playerSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S))
+            player.transform.position -= player.transform.forward * playerSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.down * playerSpeed * Time.deltaTime * 30);
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.up * playerSpeed * Time.deltaTime * 30);
+    }
+}
