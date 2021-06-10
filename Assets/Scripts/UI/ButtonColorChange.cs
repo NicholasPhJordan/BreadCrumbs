c87caae8abe15c354c8812286fe51bdfa28b4b81
Nicholas Jordan <NicholasPhJordan@gmail.com>
2021-06-09 22:25:37 -0500

    start menu buttons work and menu has art and pause menu works with esc

---
+++ Assets/Scripts/UI/ButtonColorChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonColorChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private Text _text;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _text.color = Color.red; //Or however you do your color
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _text.color = Color.white; //Or however you do your color 
+    }
+}
---
+++ Assets/Scripts/UI/MenuBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _pauseMenuUI;
+    public static bool GamePaused = false;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    //Update is called once per frame
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (GamePaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        _pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        _pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+}
