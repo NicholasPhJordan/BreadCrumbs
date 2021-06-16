63af599955499afa6f129c881696b470c411f00f
Joshua Albe <joshjalbe@gmail.com>
2021-06-16 16:52:43 -0500

    Made cut-off pursuit destination

--- Assets/scripts/Enemy/EnemyChase.cs
+++ Assets/scripts/Enemy/EnemyChase.cs
@@ -24,7 +24,11 @@ public class EnemyChase : MonoBehaviour
     [SerializeField]
     private float _trackingTimer;
     private float _timer;
-    private int _case;
+    private Vector3 _homePosition;
+    private Vector3 _pursuitDestination;
+    private Vector3 _targetLastPosition;
+
+    private bool _isCuttingOff;
 
     public GameObject TargetInUse
     {
@@ -38,6 +42,18 @@ public class EnemyChase : MonoBehaviour
         }
     }
 
+    public Vector3 EnemyPosition
+    {
+        get
+        {
+            return _agent.transform.position;
+        }
+        set
+        {
+            _agent.transform.position = _homePosition;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -45,8 +61,12 @@ public class EnemyChase : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
         _agent = GetComponent<NavMeshAgent>();
 
+        _homePosition = _agent.transform.position;
+        _isCuttingOff = true;
         _agent.speed = _runSpeed;
         _timer = _trackingTimer;
+
+        StartCoroutine(PastPositionUpdate());
     }
 
     private void FixedUpdate()
@@ -64,15 +84,27 @@ public class EnemyChase : MonoBehaviour
 
         SpeedAndTiming();
 
-        //Set the target's destination for the navmesh
-        _agent.SetDestination(_targetInUse.transform.position);
+        if(!_isCuttingOff)
+        {
+            //Set the target's destination for the navmesh
+            _agent.SetDestination(_targetInUse.transform.position);
+        }
+        else
+        {
+            PursuitCutOff();
+        }
+        
     }
 
     private void SpeedAndTiming()
     {
         //If the target is within a radius from the target, and its speed is greater than 1
-        if (_agent.remainingDistance < 5.5 && _agent.remainingDistance > 0.2 && _agent.speed > 2)
+        if (_agent.remainingDistance < 5.5 && _agent.remainingDistance > 0.2 && _agent.speed > 1.5)
         {
+            //Make the enemy chase directly
+            if (_isCuttingOff)
+                _isCuttingOff = false;
+
             //reduce speed by the speed modifier
             _agent.speed -= _speedModifier;
             //Reset the timer in case it was modified
@@ -80,12 +112,16 @@ public class EnemyChase : MonoBehaviour
 
 
             //If the agent's speed somehow drops below 1, catch it
-            if (_agent.speed < 2)
-                _agent.speed = 2f;
+            if (_agent.speed < 1.5)
+                _agent.speed = 1.5f;
         }
         //If the agent is outside the radius while still having a decreased speed
         else if (_agent.remainingDistance > 5.5 && _agent.speed < _runSpeed)
         {
+            //Make the enemy start the pursuit
+            if (!_isCuttingOff)
+                _isCuttingOff = true;
+
             //Tick down the timer
             _timer -= 0.1f;
 
@@ -102,5 +138,19 @@ public class EnemyChase : MonoBehaviour
                 _timer = _trackingTimer;
             }
         }
+    }
+
+    private void PursuitCutOff()
+    {
+        //Take the velocity and use it to predict where the target is moving
+        _pursuitDestination = _targetInUse.transform.position - _targetLastPosition;
+
+        _agent.SetDestination(_pursuitDestination);
+    }
+
+    private IEnumerator PastPositionUpdate()
+    {
+        _targetLastPosition = _targetInUse.transform.position;
+        yield return new WaitForSeconds(1.5f);
     }
 }
