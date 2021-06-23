8a7bea179b782472f01cfbd5dc8e9f65fb3baf99
Joshua Albe <joshjalbe@gmail.com>
2021-06-23 12:48:45 -0500

    Began implementing animation, just need player speed reference

---
+++ Assets/Scripts/PlayerScripts/Gameplay/PlayerAnimationBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationBehavior : MonoBehaviour
+{
+    [Tooltip("The animator attached to the player.")]
+    [SerializeField]
+    private Animator _playerAnimator;
+
+    public float testSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _playerAnimator.SetFloat("Speed", testSpeed);
+    }
+}
