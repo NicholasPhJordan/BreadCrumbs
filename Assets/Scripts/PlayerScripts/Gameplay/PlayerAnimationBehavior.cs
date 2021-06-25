096d1502bdc0d6407a90762509619bf1fe9bd60c
Joshua Albe <joshjalbe@gmail.com>
2021-06-25 09:45:17 -0500

    Fixed animations, working exactly as intended

--- Assets/Scripts/PlayerScripts/Gameplay/PlayerAnimationBehavior.cs
+++ Assets/Scripts/PlayerScripts/Gameplay/PlayerAnimationBehavior.cs
@@ -2,23 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Controller))]
+
 public class PlayerAnimationBehavior : MonoBehaviour
 {
     [Tooltip("The animator attached to the player.")]
     [SerializeField]
     private Animator _playerAnimator;
 
-    public float testSpeed;
+    private Controller _controller;
+
+    //public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _controller = GetComponent<Controller>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _playerAnimator.SetFloat("Speed", testSpeed);
+        _playerAnimator.SetFloat("Speed", _controller.GetPlayerSpeed);
     }
 }
